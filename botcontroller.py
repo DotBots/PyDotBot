29ed721f98e5304b5bf7da3f0ef830396f81b538
Somekindofa <93818867+Somekindofa@users.noreply.github.com>
2022-07-06 12:25:15 +0200

    #1. Sending every 4 axes of the joysticks

--- botcontroller.py
+++ botcontroller.py
@@ -1,16 +1,12 @@
 import base64
-import requests
-import pygame
-import os
 from enum import Enum
+import os
+import pygame
+import requests
 import time
-import struct
-import numpy as np
-from bottle import route, run, template, Bottle
 
 
-DOTBOT_GATEWAY_URL = os.getenv("DOTBOT_GATEWAY_URL", "http://localhost:8080/dotbot")
-ACTIVE_KEYS = []
+DOTBOT_GATEWAY_URL = os.getenv("DOTBOT_GATEWAY_URL", "http://127.0.0.1:8080/dotbot")
 
 
 class Command(Enum):
@@ -18,56 +14,54 @@ class Command(Enum):
     RGB_LED = 1
 
 
-def parse_speeds(left_joystick_y, right_joystick_x):
-    payload = bytearray()                                                   # init payload
-    payload += (0).to_bytes(1, 'little')                                    # version 0
-    payload += int(Command.MOVE_RAW.value).to_bytes(1, 'little')            # command type (move)
-    payload += (0).to_bytes(1, 'little', signed=True)                       # left_x = 0
-    payload += int(left_joystick_y).to_bytes(1, 'little', signed=True)      # left_y
-    payload += int(right_joystick_x).to_bytes(1, 'little', signed=True)     # right_x
-    payload += (0).to_bytes(1, 'little', signed=True)                       # right_y = 0
+def parse_speeds(left_joystick_x, left_joystick_y, right_joystick_x, right_joystick_y):
+    payload = bytearray()                                                       # init payload
+    payload += (0).to_bytes(1, 'little')                                        # version 0
+    payload += int(Command.MOVE_RAW.value).to_bytes(1, 'little')                # command type (move)
+    payload += int(left_joystick_x).to_bytes(1, 'little', signed=True)          # left_x
+    payload += int(left_joystick_y).to_bytes(1, 'little', signed=True)          # left_y
+    payload += int(right_joystick_x).to_bytes(1, 'little', signed=True)         # right_x
+    payload += int(right_joystick_y).to_bytes(1, 'little', signed=True)         # right_y
     return payload
 
 
 def send_payload(payload):
-    time1 = time.time()
-    payload_encoded = base64.b64encode(payload).decode()
-    print(payload_encoded)
-    command = {"cmd": payload_encoded}
-
-    requests.post(DOTBOT_GATEWAY_URL, json=command)
-    time2 = time.time()
-
-    print("time = ", time2 - time1)
+    payload_encoded = base64.b64encode(payload).decode()                        # configure the payload
+    command = {"cmd": payload_encoded}                                          # configure the command for the payload
+    requests.post(DOTBOT_GATEWAY_URL, json=command)                             # send the request over HTTP
 
 
-def speeds_from_keys():
-    pygame.event.pump()
-    lj_y = ps4.get_axis(1)
+def speeds_from_joysticks():
+    pygame.event.pump()             # queue needs to be pumped
+    lj_x = ps4.get_axis(0)
+    lj_y = - ps4.get_axis(1)
     rj_x = ps4.get_axis(2)
+    rj_y = - ps4.get_axis(3)
 
+    # dead zones
+    if -0.09 < lj_x <= 0.09:
+        lj_x = 0.0
     if -0.09 < lj_y <= 0.09:
         lj_y = 0.0
     if -0.09 < rj_x <= 0.09:
         rj_x = 0.0
-    lj_y = lj_y * 128
-    rj_x = rj_x * 128
-    print("lj_y: ", lj_y, 'rj_x', rj_x)
-    return lj_y, rj_x
+    if -0.09 < rj_y <= 0.09:
+        rj_y = 0.0
+
+    lj_x = lj_x * 127
+    lj_y = lj_y * 127
+    rj_x = rj_x * 127
+    rj_y = rj_y * 127
+    return lj_x, lj_y, rj_x, rj_y
 
 
 if __name__ == "__main__":
     pygame.init()
     pygame.joystick.init()
-    joystick_count = pygame.joystick.get_count()
     ps4 = pygame.joystick.Joystick(0)
     ps4.init()
-    axes = ps4.get_numaxes()
-
     while True:
-        (a, b) = speeds_from_keys()
-        payload = parse_speeds(a, b)
-        send_payload(payload)
-
-
-
+        (speed_lj_x, speed_lj_y, speed_rj_x, speed_rj_y) = speeds_from_joysticks()  # fetch positions from joysticks
+        payload = parse_speeds(speed_lj_x, speed_lj_y, speed_rj_x, speed_rj_y)      # configure the payload
+        send_payload(payload)                                                       # send the payload
+        time.sleep(0.05)                                                            # 50ms delay between each update
