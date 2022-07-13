031f58628a2af3724fd1e1fe65870c57f4857039
Somekindofa <93818867+Somekindofa@users.noreply.github.com>
2022-07-13 14:34:02 +0200

    #1. server.py: removed 'bottle' import botcontroller.py: added comments

--- botcontroller.py
+++ botcontroller.py
@@ -10,12 +10,12 @@ DOTBOT_GATEWAY_URL = os.getenv("DOTBOT_GATEWAY_URL", "http://127.0.0.1:8080/dotb
 
 
 class Command(Enum):
-    MOVE_RAW = 0
-    RGB_LED = 1
+    MOVE_RAW    = 0
+    RGB_LED     = 1
 
 
 def parse_speeds(left_joystick_x, left_joystick_y, right_joystick_x, right_joystick_y):
-    payload = bytearray()                                                       # init payload
+    payload  = bytearray()                                                      # init payload
     payload += (0).to_bytes(1, 'little')                                        # version 0
     payload += int(Command.MOVE_RAW.value).to_bytes(1, 'little')                # command type (move)
     payload += int(left_joystick_x).to_bytes(1, 'little', signed=True)          # left_x
@@ -33,10 +33,11 @@ def send_payload(payload):
 
 def pos_from_joysticks():
     pygame.event.pump()             # queue needs to be pumped
-    lj_x = ps4.get_axis(0)
-    lj_y = - ps4.get_axis(1)
-    rj_x = ps4.get_axis(2)
-    rj_y = - ps4.get_axis(3)
+
+    lj_x = ps4.get_axis(0)          # left joystick x-axis
+    lj_y = - ps4.get_axis(1)        # left joystick y-axis
+    rj_x = ps4.get_axis(2)          # right joystick x-axis
+    rj_y = - ps4.get_axis(3)        # right joystick y-axis
 
     # dead zones
     if -0.09 < lj_x <= 0.09:
@@ -48,7 +49,8 @@ def pos_from_joysticks():
     if -0.09 < rj_y <= 0.09:
         rj_y = 0.0
 
-    lj_x = lj_x * 127                   # from [-1;1] to [-127;127]
+    # from [-1;1] to [-127;127]
+    lj_x = lj_x * 127
     lj_y = lj_y * 127
     rj_x = rj_x * 127
     rj_y = rj_y * 127
@@ -56,13 +58,13 @@ def pos_from_joysticks():
 
 
 if __name__ == "__main__":
-    rj_y_speed = 0
-    pygame.init()
-    pygame.joystick.init()
-    ps4 = pygame.joystick.Joystick(0)
-    ps4.init()
+    rj_y_speed = 0                      # right joystick y-axis to be sent in payload
+    pygame.init()                       # pygame initialization
+    pygame.joystick.init()              # joysticks initialization
+    ps4 = pygame.joystick.Joystick(0)   # instantiation of a joystick
+    ps4.init()                          # initialization of the joystick
     while True:
         (pos_lj_x, pos_lj_y, pos_rj_x, pos_rj_y) = pos_from_joysticks()             # fetch positions from joysticks
-        payload = parse_speeds(pos_lj_x, pos_lj_y, pos_rj_x, pos_rj_y)            # configure the payload
+        payload = parse_speeds(pos_lj_x, pos_lj_y, pos_rj_x, pos_rj_y)              # configure the payload
         send_payload(payload)                                                       # send the payload
         time.sleep(0.05)                                                            # 50ms delay between each update
