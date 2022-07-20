f77c5e8e55e800fc326aef10af9b8ec147505d9c
Somekindofa <93818867+Somekindofa@users.noreply.github.com>
2022-07-20 16:55:10 +0200

    #1. Cleaned code. Added system call exit()

--- botcontroller.py
+++ botcontroller.py
@@ -4,10 +4,10 @@ import time
 from enum import Enum
 import pygame
 import requests
+import sys
 
 
 DOTBOT_GATEWAY_URL = os.getenv("DOTBOT_GATEWAY_URL", "http://127.0.0.1:8080/dotbot")
-
 
 JOYSTICK_HYSTERERIS_THRES = 0.09
 
@@ -17,7 +17,7 @@ class Command(Enum):
     RGB_LED     = 1
 
 
-def payload_from_positions (left_joystick_x, left_joystick_y, right_joystick_x, right_joystick_y):
+def payload_from_positions(left_joystick_x, left_joystick_y, right_joystick_x, right_joystick_y):
     payload  = bytearray()                                                      # init payload
     payload += (0).to_bytes(1, 'little')                                        # version 0
     payload += int(Command.MOVE_RAW.value).to_bytes(1, 'little')                # command type (move)
@@ -35,22 +35,22 @@ def send_payload(payload):
 
 
 def pos_from_joystick(joystick):
-    pygame.event.pump()             # queue needs to be pumped
-    if joystick.get_numaxes() < 5:
-        exit()
-    lj_x = joystick.get_axis(0)          # left joystick x-axis
-    lj_y = - joystick.get_axis(1)        # left joystick y-axis
-    rj_x = joystick.get_axis(2)          # right joystick x-axis
-    rj_y = - joystick.get_axis(3)        # right joystick y-axis
+    pygame.event.pump()                     # queue needs to be pumped
+    if joystick.get_numaxes() < 4:
+        sys.exit("Not enough axes on your joystick. {} found".format(joystick.get_numaxes()))
+    lj_x = joystick.get_axis(0)             # left joystick x-axis
+    lj_y = - joystick.get_axis(1)           # left joystick y-axis
+    rj_x = joystick.get_axis(2)             # right joystick x-axis
+    rj_y = - joystick.get_axis(3)           # right joystick y-axis
 
     # dead zones
-    if -JOYSTICK_HYSTERERIS_THRES < lj_x <= JOYSTICK_HYSTERERIS_THRES :
+    if -JOYSTICK_HYSTERERIS_THRES < lj_x <= JOYSTICK_HYSTERERIS_THRES:
         lj_x = 0.0
-    if -JOYSTICK_HYSTERERIS_THRES < lj_y <= JOYSTICK_HYSTERERIS_THRES :
+    if -JOYSTICK_HYSTERERIS_THRES < lj_y <= JOYSTICK_HYSTERERIS_THRES:
         lj_y = 0.0
-    if -JOYSTICK_HYSTERERIS_THRES < rj_x <= JOYSTICK_HYSTERERIS_THRES :
+    if -JOYSTICK_HYSTERERIS_THRES < rj_x <= JOYSTICK_HYSTERERIS_THRES:
         rj_x = 0.0
-    if -JOYSTICK_HYSTERERIS_THRES < rj_y <= JOYSTICK_HYSTERERIS_THRES :
+    if -JOYSTICK_HYSTERERIS_THRES < rj_y <= JOYSTICK_HYSTERERIS_THRES:
         rj_y = 0.0
 
     # from [-1;1] to [-127;127]
@@ -60,12 +60,13 @@ def pos_from_joystick(joystick):
     rj_y = rj_y * 127
     return lj_x, lj_y, rj_x, rj_y
 
+
 def main():
     pygame.init()                       # pygame initialization
     pygame.joystick.init()              # joysticks initialization
-    ps4 = pygame.joystick.Joystick(0)   # instantiation of a joystick
     if pygame.joystick.get_count() == 0:
-        exit()
+        sys.exit("Error: No joystick connected.\nExiting program...")
+    ps4 = pygame.joystick.Joystick(0)   # instantiation of a joystick
     ps4.init()                          # initialization of the joystick
     while True:
         pos_lj_x, pos_lj_y, pos_rj_x, pos_rj_y = pos_from_joystick(ps4)                 # fetch positions from joysticks
