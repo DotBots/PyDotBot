fffc70506b60f01dc1117da7b4ec3aa526fbe65d
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-09-07 10:01:53 +0200

    bot_controller/factory: add partial doctest

--- bot_controller/factory.py
+++ bot_controller/factory.py
@@ -10,7 +10,16 @@ class ControllerException(Exception):
 
 
 def controller_factory(type_, port, baudrate):
-    """Returns an instance of a concrete Dotbot controller."""
+    """Returns an instance of a concrete Dotbot controller.
+
+    >>> controller_factory("invalid", "/dev/tty", 115200)
+    Traceback (most recent call last):
+    bot_controller.factory.ControllerException: Invalid controller
+    >>> controller_factory("keyboard", "/dev/tty", 115200)  # doctest: +ELLIPSIS
+    <bot_controller.keyboard.KeyboardController object at 0x...>
+    >>> controller_factory("server", "/dev/tty", 115200)  # doctest: +ELLIPSIS
+    <bot_controller.server.ServerController object at 0x...>
+    """
     if type_ == "keyboard":
         return KeyboardController(port, baudrate)
     if type_ == "joystick":
