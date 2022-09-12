fe7520861b400eaa7fda761bd3cf9e090e991590
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-09-12 16:12:06 +0200

    hdlc: add function to encode payload in an hdlc frame

--- bot_controller/bc_serial.py
+++ bot_controller/bc_serial.py
@@ -1,11 +1,11 @@
 """Dotbot controller serial interface."""
 
 import serial
+from bot_controller.hdlc import hdlc_encode
 
 
 def write(serial_port: str, serial_baudrate: int, message: bytes):
     """Write bytes on a serial port."""
 
     with serial.Serial(serial_port, serial_baudrate, timeout=1) as ser:
-        ser.write(len(message).to_bytes(1, "little"))
-        ser.write(message)
+        ser.write(hdlc_encode(message))
