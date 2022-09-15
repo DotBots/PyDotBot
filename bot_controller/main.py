6c2c53388433d1b3f96a9370957e5c1593015878
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-09-15 11:46:54 +0200

    main: use click instead of argparse

--- bot_controller/main.py
+++ bot_controller/main.py
@@ -2,11 +2,11 @@
 
 """Main module of the Dotbot controller command line tool."""
 
-import argparse
 import sys
 
 from importlib.metadata import version, PackageNotFoundError
 
+import click
 import serial
 
 from bot_controller.controller import (
@@ -24,36 +24,30 @@ SERIAL_BAUDRATE_DEFAULT = 1000000
 CONTROLLER_TYPE_DEFAULT = "keyboard"
 
 
-def main():
-    """Main function."""
-
-    parser = argparse.ArgumentParser(
-        description="BotController, universal SailBot and DotBot controller"
-    )
-    parser.add_argument(
-        "-t",
-        "--type",
-        help='Type of your controller. Defaults to "keyboard"',
-        type=str,
-        choices=["joystick", "keyboard", "server"],
-        default=CONTROLLER_TYPE_DEFAULT,
-    )
-    parser.add_argument(
-        "-p",
-        "--port",
-        help='Linux users: path to port in "/dev" folder ; Windows users: COM port. Defaults to "/dev/ttyACM0"',
-        type=str,
-        default=SERIAL_PORT_DEFAULT,
-    )
-    parser.add_argument(
-        "-b",
-        "--baudrate",
-        help="Serial baudrate. Defaults to 1000000",
-        type=int,
-        default=SERIAL_BAUDRATE_DEFAULT,
-    )
-    args = parser.parse_args()
-
+@click.command()
+@click.option(
+    "-t",
+    "--type",
+    type=click.Choice(["joystick", "keyboard", "server"]),
+    default=CONTROLLER_TYPE_DEFAULT,
+    help="Type of your controller. Defaults to 'keyboard'",
+)
+@click.option(
+    "-p",
+    "--port",
+    type=str,
+    default=SERIAL_PORT_DEFAULT,
+    help=f"Linux users: path to port in '/dev' folder ; Windows users: COM port. Defaults to '{SERIAL_PORT_DEFAULT}'",
+)
+@click.option(
+    "-b",
+    "--baudrate",
+    type=int,
+    default=SERIAL_BAUDRATE_DEFAULT,
+    help=f"Serial baudrate. Defaults to {SERIAL_BAUDRATE_DEFAULT}",
+)
+def main(type, port, baudrate):
+    """BotController, universal SailBot and DotBot controller."""
     # welcome sentence
     try:
         package_version = version("dotbot_controller")
@@ -68,7 +62,7 @@ def main():
     register_controller("server", ServerController)
 
     try:
-        controller = controller_factory(args.type, args.port, args.baudrate)
+        controller = controller_factory(type, port, baudrate)
         controller.start()
     except ControllerException:
         sys.exit("Invalid controller type.")
