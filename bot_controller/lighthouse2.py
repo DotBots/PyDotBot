lighthouse: frontend: fix coordinates computation and parametrize map size

bot_controller/lighthouse2.py
@@ -100,10 +100,10 @@ END_BUFFERS = [
     ],
 ]
 REFERENCE_POINTS_DEFAULT = [
-    [-0.2, 0.2],
-    [0.2, 0.2],
-    [-0.2, -0.2],
-    [0.2, -0.2],
+    [-0.1, 0.1],
+    [0.1, 0.1],
+    [-0.1, -0.1],
+    [0.1, -0.1],
 ]
 
 
@@ -225,8 +225,7 @@ class LighthouseManager:  # pylint: disable=too-many-instance-attributes
         )
         self.calibration_data = self._load_calibration()
         self.calibration_points = np.zeros(
-            (2, len(self.reference_points), 2),
-            dtype=np.float64
+            (2, len(self.reference_points), 2), dtype=np.float64
         )
         self.calibration_points_available = [False] * len(self.reference_points)
         self.last_raw_data = None
@@ -343,7 +342,7 @@ class LighthouseManager:  # pylint: disable=too-many-instance-attributes
 
         M, _ = cv2.findHomography(
             final_points.dot(random_rodriguez.T)[:, 0:2],
-            np.array([self.reference_points], dtype=np.float64),
+            np.array([self.reference_points], dtype=np.float64) + 0.5,
             cv2.RANSAC,
             5.0,
         )