18cca273e792af57abf0170d3c64307f4fdd512b
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-02-15 15:31:23 +0100

    frontend: add UI and logic for handling waypoint target threshold

--- dotbot/frontend/src/constants.js
+++ dotbot/frontend/src/constants.js
@@ -11,6 +11,4 @@ export const ControlModeType = {
 export const maxWaypoints = 8;
 
 export const lh2_distance_threshold = 0.01;
-export const lh2_waypoint_area_radius = 0.07;
 export const gps_distance_threshold = 10;  // 10 meters
-export const gps_waypoint_area_radius = 15; // 15 meters
