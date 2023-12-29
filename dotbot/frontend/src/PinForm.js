1ec00bf388a8049184f9d4637fbec6bd1bc4a586
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-12-29 16:55:01 +0100

    mqtt: implement request/reply over MQTT, clean PinForm

---
+++ dotbot/frontend/src/PinForm.js
@@ -0,0 +1,23 @@
+import './PinForm.css';
+
+const PinForm = ({ pinUpdate }) => {
+
+  const onInputPinChange = (event) => {
+    if (event.target.value.length === 8) {
+      pinUpdate(event.target.value);
+    }
+  };
+
+  return (
+    <div className="container">
+      <form id="pin-code-input">
+        <p>Enter pin code:</p>
+        <p>
+          <input type="password" className="form-control" autoFocus="autofocus" onChange={(event) => onInputPinChange(event)} />
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default PinForm;
