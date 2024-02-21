1455abffe2a0492cde52e97a4a8b82e597215780
Geovane Fedrecheski <geonnave@gmail.com>
2024-02-21 12:19:27 +0100

    edhoc: format code with black

--- utils/falso_edhoc_initiator.py
+++ utils/falso_edhoc_initiator.py
@@ -62,7 +62,10 @@ class FalsoBot:
     def parse_serial_input(self, frame):
         payload = ProtocolPayload.from_bytes(hdlc_decode(frame))
 
-        if self.address == hex(payload.header.destination)[2:] and payload.payload_type == PayloadType.EDHOC_MESSAGE:
+        if (
+            self.address == hex(payload.header.destination)[2:]
+            and payload.payload_type == PayloadType.EDHOC_MESSAGE
+        ):
             print("received payload:", payload.values.value)
             return payload.values.value
         else:
@@ -100,11 +103,14 @@ class EdhocBot:
         print("Authz voucher is valid!")
         self.initiator.verify_message_2(self.PRIV_I, self.CRED_I, valid_cred_r)
         print("Message 2 is valid!")
-        message_3, i_prk_out = self.initiator.prepare_message_3(lakers.CredentialTransfer.ByReference, None)
+        message_3, i_prk_out = self.initiator.prepare_message_3(
+            lakers.CredentialTransfer.ByReference, None
+        )
         ser.write(self.fb.edhoc_message(message_3))
         time.sleep(1)
         ser.write(self.fb.advertise())
         print("sent msg3 and advertise")
+
 
 # open serial port
 ser = serial.Serial(sys.argv[1], timeout=1)
@@ -114,18 +120,28 @@ ID_U = bytes.fromhex("a104412b")
 G_W = bytes.fromhex("FFA4F102134029B3B156890B88C9D9619501196574174DCB68A07DB0588E4D41")
 LOC_W = "http://localhost:18000"
 
-CRED_I = bytes.fromhex("A2027734322D35302D33312D46462D45462D33372D33322D333908A101A5010202412B2001215820AC75E9ECE3E50BFC8ED60399889522405C47BF16DF96660A41298CB4307F7EB62258206E5DE611388A4B8A8211334AC7D37ECB52A387D257E6DB3C2A93DF21FF3AFFC8")
-PRIV_I = bytes.fromhex("fb13adeb6518cee5f88417660841142e830a81fe334380a953406a1305e8706b")
+CRED_I = bytes.fromhex(
+    "A2027734322D35302D33312D46462D45462D33372D33322D333908A101A5010202412B2001215820AC75E9ECE3E50BFC8ED60399889522405C47BF16DF96660A41298CB4307F7EB62258206E5DE611388A4B8A8211334AC7D37ECB52A387D257E6DB3C2A93DF21FF3AFFC8"
+)
+PRIV_I = bytes.fromhex(
+    "fb13adeb6518cee5f88417660841142e830a81fe334380a953406a1305e8706b"
+)
 
 edhoc_bots = [
     EdhocBot(
-        PRIV_I, CRED_I,
-        bytes.fromhex("a104412b"), G_W, LOC_W,
+        PRIV_I,
+        CRED_I,
+        bytes.fromhex("a104412b"),
+        G_W,
+        LOC_W,
     ),
     EdhocBot(
-        PRIV_I, CRED_I,
-        bytes.fromhex("a104413c"), G_W, LOC_W,
-    )
+        PRIV_I,
+        CRED_I,
+        bytes.fromhex("a104413c"),
+        G_W,
+        LOC_W,
+    ),
 ]
 
 for edhoc_bot in edhoc_bots:
