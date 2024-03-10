2b47b1de40ddb4a93a41081a35cc568890283726
bkorecic <blaz.korecic@ug.uchile.cl>
2024-03-10 20:27:54 +0100

    Modify configs

--- dotbot/new-frontend/.eslintrc.cjs
+++ dotbot/new-frontend/.eslintrc.cjs
@@ -3,17 +3,23 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/prefer-optional-chain': 'off',
   },
 }
+
