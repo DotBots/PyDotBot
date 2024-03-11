d9da1a2a7519c8454e3e3c6517503e754a0b495e
bkorecic <blaz.korecic@ug.uchile.cl>
2024-03-11 18:43:50 +0100

    Add placeholder code for buttons

--- dotbot/new-frontend/src/App.tsx
+++ dotbot/new-frontend/src/App.tsx
@@ -1,26 +1,80 @@
-import useDotBots from './useDotBots'
-import { DotBotModel } from './models'
+import useDotBots from "./useDotBots";
+import { DotBotModel } from "./models";
+import { InteractiveSVGContainer } from "./InteractiveSVGContainer";
+import {
+  IconButton,
+  Grid,
+  GridItem,
+  Center,
+  HStack,
+  VStack,
+  Tooltip,
+} from "@chakra-ui/react";
+import {
+  AiOutlineSetting,
+  AiOutlineCompass,
+  AiOutlineCaretRight,
+  AiOutlinePause,
+} from "react-icons/ai";
+import { DotBotList } from "./menu/DotBotList";
 
 function App() {
-
   const { dotBots } = useDotBots();
 
   return (
-    <>
-      <div>
-        <p>DotBot info</p>
-        { dotBots && dotBots.map((dotBot: DotBotModel, index: number) => {
-          return (
-            <div key={index}>
-              <div>DotBot {index + 1}</div>
-              <div>Address: {dotBot.address}</div>
-              <div>Position: {dotBot.lh2_position && <b>{dotBot.lh2_position.x}, {dotBot.lh2_position.y}</b>}</div>
-            </div>
-          )
-        })}
-      </div>
-    </>
-  )
+    <Center>
+      <Grid
+        // Map positions of the grid. t = top, l = left, c = center, r = right, b = bottom
+        templateAreas={`"tl t tr"
+                        "l  c r"
+                        "bl b br"`}
+        gridTemplateColumns="auto 1fr auto"
+        gridTemplateRows="auto 1fr auto"
+        gap="1"
+      >
+        <GridItem gridArea="l">
+          <VStack>
+            <DotBotList dotBots={dotBots} />
+          </VStack>
+        </GridItem>
+        <GridItem gridArea="c">
+          <InteractiveSVGContainer width={500} height={500} />
+        </GridItem>
+        <GridItem gridArea="b">
+          <HStack h="100%">
+            <Tooltip hasArrow label="Map settings">
+              <IconButton
+                aria-label="Map settings"
+                fontSize="25px"
+                icon={<AiOutlineSetting />}
+              />
+            </Tooltip>
+            <Tooltip hasArrow label="Start/stop calibration">
+              <IconButton
+                aria-label="Start/stop calibration"
+                fontSize="25px"
+                icon={<AiOutlineCompass />}
+              />
+            </Tooltip>
+            <Tooltip hasArrow label="Start DotBot movement">
+              <IconButton
+                aria-label="Start DotBot movement"
+                fontSize="25px"
+                icon={<AiOutlineCaretRight />}
+              />
+            </Tooltip>
+            <Tooltip hasArrow label="Stop DotBot movement">
+              <IconButton
+                aria-label="Stop DotBot movement"
+                fontSize="25px"
+                icon={<AiOutlinePause />}
+              />
+            </Tooltip>
+          </HStack>
+        </GridItem>
+      </Grid>
+    </Center>
+  );
 }
 
-export default App
+export default App;
