861b0a64182f1b5ac31234c8e266dec834b1344b
Alexandre Abadie <alexandre.abadie@inria.fr>
2024-05-16 14:33:05 +0200

    frontend/XGOItem: integrate JoyStick and color selector

--- dotbot/frontend/src/XGOItem.js
+++ dotbot/frontend/src/XGOItem.js
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import { dotbotStatuses, dotbotBadgeStatuses } from "./utils/constants";
 
+import { RgbColorPicker } from "react-colorful";
+import { Joystick } from "./Joystick";
 import { XGOActionId } from "./utils/constants";
 import logger from './utils/logger';
 const log = logger.child({module: 'xgo-item'});
 
 export const XGOItem = ({dotbot, updateActive, publishCommand}) => {
 
-  let rgbColor = "rgb(0, 0, 0)"
+  const [ expanded, setExpanded ] = useState(false);
+  const [ color, setColor ] = useState({ r: 0, g: 0, b: 0 });
+
+  const applyColor = async () => {
+    log.info(`Applying color ${color.r}, ${color.g}, ${color.b}`);
+    await publishCommand(dotbot.address, dotbot.application, "rgb_led", { red: color.r, green: color.g, blue: color.b });
+  }
 
   const applyAction = async (action) => {
     log.info(`Applying XGO action ${action}`);
     await publishCommand(dotbot.address, dotbot.application, "xgo_action", { action: action });
   }
+
+  let rgbColor = "rgb(0, 0, 0)"
+  if (dotbot.rgb_led) {
+    rgbColor = `rgb(${dotbot.rgb_led.red}, ${dotbot.rgb_led.green}, ${dotbot.rgb_led.blue})`
+  }
+
+  useEffect(() => {
+    if (dotbot.rgb_led) {
+      setColor({ r: dotbot.rgb_led.red, g: dotbot.rgb_led.green, b: dotbot.rgb_led.blue })
+    } else {
+      setColor({ r: 0, g: 0, b: 0 })
+    }
+
+    // Add collapse/expand event listener to avoid weird effects with the joystick
+    let collapsibleElement = document.getElementById(`collapse-${dotbot.address}`)
+    collapsibleElement.addEventListener('hide.bs.collapse', () => {
+      setExpanded(false);
+    })
+    collapsibleElement.addEventListener('shown.bs.collapse', () => {
+      setExpanded(true);
+    })
+  }, [dotbot.address, dotbot.rgb_led, setColor, setExpanded]);
 
   return (
     <div className="accordion-item">
@@ -34,6 +65,19 @@ export const XGOItem = ({dotbot, updateActive, publishCommand}) => {
       </h2>
       <div id={`collapse-${dotbot.address}`} className="accordion-collapse collapse" aria-labelledby={`heading-${dotbot.address}`} data-bs-parent="#accordion-xgo">
         <div className="accordion-body">
+          <div className="d-flex">
+            <div className={`mx-auto justify-content-center ${!expanded && "invisible"}`}>
+              <Joystick address={dotbot.address} application={dotbot.application} publishCommand={publishCommand} />
+            </div>
+            <div className="mx-auto justify-content-center">
+              <div className="d-flex justify-content-center">
+                <RgbColorPicker color={color} onChange={setColor} />
+              </div>
+              <div className="d-flex justify-content-center">
+                <button className="btn btn-primary m-1" onClick={applyColor}>Apply color</button>
+              </div>
+            </div>
+          </div>
           <div className="d-flex mx-auto card">
             <div className="card-body p-1">
               <p className="m-0 p-0">
